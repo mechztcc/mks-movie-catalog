2f69ccfb09e23afd5e54eb4efd987ebac96fee96
mechztcc <dojathan@gmail.com>
2023-11-27 09:28:34 -0500

    feat: add redis to cache movies

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,13 @@ import { Movie } from './modules/movies/entities/movie.entity';
 import { MoviesModule } from './modules/movies/movies.module';
 import { User } from './modules/users/entities/user.entity';
 import { UsersModule } from './modules/users/users.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -19,6 +23,12 @@ import { UsersModule } from './modules/users/users.module';
       database: process.env.DATABASE,
       entities: [User, Movie],
       synchronize: false,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
     UsersModule,
     Auth,
--- src/modules/movies/services/index-movies/index-movies.service.ts
+++ src/modules/movies/services/index-movies/index-movies.service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { Movie } from '../../entities/movie.entity';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
 import { Repository } from 'typeorm';
-import { User } from 'src/modules/users/entities/user.entity';
+import { Movie } from '../../entities/movie.entity';
 
 @Injectable()
 export class IndexMoviesService {
   constructor(
     @InjectRepository(Movie)
     private moviesRepository: Repository<Movie>,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
   ) {}
 
   async execute(): Promise<any> {
+    const moviesExists = await this.cacheService.get('movies');
+    if (moviesExists) {
+      console.log('GETTING FROM REDIS');
+      return moviesExists;
+    }
+
     const movies = await this.moviesRepository.find();
 
     return movies;
