ae1534e13e569e454f8a4c1f58270e4c48460802
mechztcc <dojathan@gmail.com>
2023-11-27 12:02:57 -0500

    fix: validate user token when try to create new movie

--- src/modules/movies/services/create-movie/create-movie.service.ts
+++ src/modules/movies/services/create-movie/create-movie.service.ts
@@ -1,10 +1,11 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cache } from 'cache-manager';
 import { Repository } from 'typeorm';
 import { CreateMovieDto } from '../../dto/create-movie.dto';
 import { Movie } from '../../entities/movie.entity';
+import { User } from 'src/modules/users/entities/user.entity';
 
 interface IRequest {
   data: CreateMovieDto;
@@ -16,10 +17,20 @@ export class CreateMovieService {
   constructor(
     @InjectRepository(Movie)
     private moviesRepository: Repository<Movie>,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
     @Inject(CACHE_MANAGER) private cacheService: Cache,
   ) {}
 
   async execute({ data, userId }: IRequest): Promise<any> {
+    const userExists = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!userExists) {
+      throw new UnauthorizedException('Provided user has not found.');
+    }
+
     const movie = this.moviesRepository.create({
       age: data.age,
       description: data.description,
